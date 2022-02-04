e53df85d27f0058cceeaa950d661a8b1a87b3f20
kris_wale <40746582+kristiankf@users.noreply.github.com>
2022-02-04 06:32:46 +0000

    Create newsForm.vue

---
+++ src/components/newsForm.vue
@@ -0,0 +1,27 @@
+<template>
+    <form>
+        <h2>NewsNews Update</h2>
+        <div>
+            <label for="Title">News Title:</label>
+            <p><input type="text" id="Title" placeholder="News title here"></p>
+        </div>
+        <div>
+            <label for="headline">News Headline</label>
+            <p><textarea id="headline" placeholder="Headline here"></textarea></p>
+        </div>
+        <div>
+            <input type="submit" value="Post">
+        </div>
+    </form>
+</template>
+
+<script>
+export default {
+    name: 'newsForm',
+    
+}
+</script>
+
+<style scoped>
+
+</style>
